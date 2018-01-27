e22ffb15f3611e9b90187eec00240304ced1f31f
Yoav Alon <31308918+yoavalon@users.noreply.github.com>
2018-01-28 00:04:37 +0200

    Creation of Neural Network

---
+++ NeuralNetworkTrafficSigns.py
@@ -0,0 +1,177 @@
+#according to an tutorial from https://www.datacamp.com/community/tutorials/tensorflow-tutorial
+import os
+import datetime
+import numpy as np
+import skimage
+import skimage.data
+from skimage import transform
+from skimage import color as col
+import tensorflow as tf
+import cv2
+import matplotlib.pyplot as plt 
+
+
+def Log(content) : 
+    print("-->", datetime.datetime.now(), content)
+    
+
+def load_data(data_directory):
+    Log("Load Data")
+    directories = [d for d in os.listdir(data_directory) if os.path.isdir(os.path.join(data_directory,d))]
+    labels = []
+    images = []
+
+    for d in directories : 
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f) for f in os.listdir(label_directory) if f.endswith(".ppm")]
+        
+        for f in file_names :
+            images.append(skimage.data.imread(f))            
+            labels.append(int(d))
+    
+    images = np.array(images)       #little fix, convert to numpy arrays for ndim
+    labels = np.array(labels)
+
+    return images, labels
+
+Log("start")
+ROOT_PATH = "C:/Users/Alon/Desktop/PythonSourceCode/"
+
+train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Training")
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+images, labels = load_data(train_data_directory)
+
+images28 = [transform.resize(image, (28, 28)) for image in images]
+# Convert `images28` to an array
+images28 = np.array(images28)
+
+# Convert `images28` to grayscale
+images28 = col.rgb2gray(images28)
+
+Log("BUild Neural Network")
+
+# Initialize placeholders 
+x = tf.placeholder(dtype = tf.float32, shape = [None, 28, 28])
+y = tf.placeholder(dtype = tf.int32, shape = [None])
+
+# Flatten the input data
+images_flat = tf.contrib.layers.flatten(x)
+
+# Fully connected layer 
+logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
+
+# Define a loss function
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, 
+                                                                    logits = logits))
+# Define an optimizer 
+train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+# Convert logits to label indexes
+correct_pred = tf.argmax(logits, 1)
+
+# Define an accuracy metric
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+print("images_flat: ", images_flat)
+print("logits: ", logits)
+print("loss: ", loss)
+print("predicted_labels: ", correct_pred)
+
+Log("end")
+
+
+
+
+
+
+# old code for test output
+
+'''
+# Print the `images` dimensions
+print(images.ndim)
+
+# Print the number of `images`'s elements
+print(images.size)
+
+# Count the number of labels
+print(len(set(labels)))
+'''
+
+'''
+# Print the first instance of `images`
+cv2.imshow("First Image", images[0])
+k = cv2.waitKey(30) & 0xff 
+
+# Make a histogram with 62 bins of the `labels` data
+plt.hist(labels, 62)
+# Show the plot -> Shows us how often each traffic sign is represented
+plt.show()   
+
+# Determine the (random) indexes of the images that you want to see 
+traffic_signs = [300, 2250, 3650, 4000]
+'''
+
+
+'''
+# Fill out the subplots with the random images that you defined 
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(images[traffic_signs[i]])
+    plt.subplots_adjust(wspace=0.5)
+
+plt.show()
+'''
+'''
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(images[traffic_signs[i]])
+    plt.subplots_adjust(wspace=0.5)
+    plt.show()
+    print("shape: {0}, min: {1}, max: {2}".format(images[traffic_signs[i]].shape, images[traffic_signs[i]].min(), images[traffic_signs[i]].max()))
+'''
+
+'''
+# Get the unique labels 
+unique_labels = set(labels)
+
+# Initialize the figure
+plt.figure(figsize=(15, 15))
+
+# Set a counter
+i = 1
+
+# For each unique label,
+for label in unique_labels:
+    # You pick the first image for each label
+    image = images[labels.index(label)]
+    # Define 64 subplots 
+    plt.subplot(8, 8, i)
+    # Don't include axes
+    plt.axis('off')
+    # Add a title to each subplot 
+    plt.title("Label {0} ({1})".format(label, labels.count(label)))
+    # Add 1 to the counter
+    i += 1
+    # And you plot this first image 
+    plt.imshow(image)
+    
+# Show the plot
+plt.show()
+'''
+#working example to show 4 traffic signs
+
+'''
+traffic_signs = [300, 2250, 3650, 4000]
+
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(images28[traffic_signs[i]], cmap="gray")
+    plt.subplots_adjust(wspace=0.5)
+    
+# Show the plot
+plt.show()
+'''
